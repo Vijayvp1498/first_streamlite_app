86f102b6a1d35f17593e0d40cfbe24d2cf224dc7
Vijayvp1498 <159750623+Vijayvp1498@users.noreply.github.com>
2024-02-13 13:10:52 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,5 +8,5 @@ streamlit.text('🏵️ Hard-Boiled Fried ')
 streamlit.title('Build Your Own fruits 🍎🍒🍉🍓🍊🥭🫒🥝')
 
 import request
-//fruityvice_response=request.get("https://fruityvice.com/api/fruit/watermelon")
-//streamlit.text(fruityvice_response.json())
+fruityvice_response=request.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response.json())
